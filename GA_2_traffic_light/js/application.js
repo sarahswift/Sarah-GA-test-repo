50359b243e78d06d5ec355ea50c5f4b949ac7f0b
Saimon Sharif <sharif.saimon@gmail.com>
2014-09-20 20:26:03 -0400

    Help with JS for traffic light. Also fixed spacing in file

--- GA_2_traffic_light/js/application.js
+++ GA_2_traffic_light/js/application.js
@@ -1,37 +1,33 @@
 function buttonClick(lightToBeTurnedOn){
 
- var yellowLight = document.getElementById("yellowLight");
- var redLight = document.getElementById("redLight");
- var greenLight = document.getElementById("greenLight"); 
-  
- yellowLight.style.backgroundColor = "black";
- redLight.style.backgroundColor = "black"; 
- greenLight.style.backgroundColor = "black";
+    // You're using classes in your index.html file, but trying to obtain the lights via their idbelow. You can either use getElementsByClassName or use id's in your markup.
+    var yellowLight = document.getElementById("yellowLight");
+    var redLight = document.getElementById("redLight");
+    var greenLight = document.getElementById("greenLight"); 
 
- if buttonClick("Go") {
- 	 greenLight.style.backgroundColor = "green";
- }
+    yellowLight.style.backgroundColor = "black";
+    redLight.style.backgroundColor = "black"; 
+    greenLight.style.backgroundColor = "black";
 
- else{
- 	greenLight.style.backgroundColor = "black";
- }
+    // "buttonClick" is our method, so we shouldn't check if buttonClick("Go") is true or false
+    // We do know that the argument lightToBeTurnedOn will be "Go" for the green light, so you were on the right track
+    // We need to be careful because you have 'go' as the argument in your index.html file but you were checking for "Go". Remember, Javascript doesn't know they're the same thing
+    if (lightToBeTurnedOn === "go") {
+        greenLight.style.backgroundColor = "green";
+    } else {
+        greenLight.style.backgroundColor = "black";
+    }
 
+    if (lightToBeTurnedOn == "caution") {
+        yellowLight.style.backgroundColor = "yellow";
+    } else {
+        yellowLight.style.backgroundColor = "black";
+    }
 
- if buttonClick("Caution") {
- 	 yellowLight.style.backgroundColor = "yellow";
- }
-
- else{
- 	yellowLight.style.backgroundColor = "black";
- }
-
-
- if buttonClick("Stop") {
- 	 redLight.style.backgroundColor = "red";
- }
-
- else{
- 	redLight.style.backgroundColor = "black";
- }
+    if (lightToBeTurnedOn == "stop") {
+        redLight.style.backgroundColor = "red";
+    } else {
+        redLight.style.backgroundColor = "black";
+    }
 
 }
