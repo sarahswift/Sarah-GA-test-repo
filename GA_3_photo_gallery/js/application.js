5dc980c0fdf618131d58b5e62ee96b5743a849f6
sarahswift <sarahharvey@sarahharvey-macbookair.roam.corp.google.com>
2014-09-30 21:16:33 -0400

    photo_gallery_v1

---
+++ GA_3_photo_gallery/js/application.js
@@ -0,0 +1,29 @@
+var images = ["img/IMG_0218 copy.jpg", "img/IMG_0221 copy.jpg", "img/IMG_0273 copy.jpg", "img/IMG_0314 copy.jpg", "img/IMG_0311 copy.jpg"]
+var currentImage = 0;
+
+
+function changePicture(whichNum) {
+	var img = document.getElementById('mainImage');
+	img.setAttribute('src', images[whichNum]);
+	currentImage = whichNum;
+
+}
+
+function nextImage() {
+	currentImage++;
+	if (currentImage===images.length){
+		currentImage=0;
+	}
+	var img = document.getElementById('mainImage');
+	img.setAttribute('src', images[currentImage]);
+}
+
+function prevImage() {
+	currentImage = currentImage - 1;
+	if (currentImage=== -1){
+		currentImage=0;
+	}
+	var img = document.getElementById('mainImage');
+	img.setAttribute('src', images[currentImage]);
+}
+
